ebc67e5625edb6f80a035387128e24fecc4a488b
resiato <resiatolaitete@gmail.com>
2022-04-28 15:52:00 +0000

    Initial commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
-"""
-Python script takes URL from stdin and compute exact metrics
-"""
-import re
+
+""" script that reads stdin line by line and computes metrics """
+
 import sys
 
 
-def print_log_parsing(dic, file_size):
-    """
-    function that print parsing logs
-    args:
-        dic: is a dictionary of status code
-        file_size: is the size of status
-    """
-    print("File size: {:d}".format(file_size))
+def printsts(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
     for i in sorted(dic.keys()):
-	if dic[i] != 0;
-           print("{}: {:d}".format(i, dic[i]))
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
 sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
        "404": 0, "405": 0, "500": 0}
 
 count = 0
-file_size = 0
+size = 0
 
 try:
     for line in sys.stdin:
         if count != 0 and count % 10 == 0:
-            printsts(sts, file_size)
+            printsts(sts, size)
 
         stlist = line.split()
         count += 1
@@ -43,9 +37,9 @@ try:
                 sts[stlist[-2]] += 1
         except:
             pass
-    printsts(sts, file_size)
+    printsts(sts, size)
 
 
 except KeyboardInterrupt:
-    printsts(sts, file_size)
+    printsts(sts, size)
     raise
