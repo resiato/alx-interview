2909086c55da1f98dfd00440b4633e0da4e97451
resiato <resiatolaitete@gmail.com>
2022-05-01 10:54:00 +0000

    [Fix] remove parameters from method

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,44 +2,39 @@
 
 """ script that reads stdin line by line and computes metrics """
 
-import sys
-
-
-def printsts(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+from sys import stdin
 
 
 sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
        "404": 0, "405": 0, "500": 0}
 
-count = 0
 size = 0
 
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
+def printsts(dic, size):
+    """ Prints information
 
-        stlist = line.split()
-        count += 1
+    Args:
+	sts (dict): code status
+	size(int): size of files
+    """
+    print("File size: {}".format(size))
+    for key, val in sorted(stats.items()):
+        if val > 0:
+            print("{}: {:d}".format(key, val))
 
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
-
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
-
-
-except KeyboardInterrupt:
-    printsts(sts, size)
-    raise
+if __name__ == '__main__':
+    try:
+       for i, line in enumerate(stdin, 1):
+            try:
+                info = line.split()
+                size += int(info[-1])
+                if info[-2] in codes.keys():
+                    codes[info[-2]] += 1
+            except:
+                pass
+            if not i % 10:
+                print_info()
+    except KeyboardInterrupt:
+        print_info()
+        raise
+    print_info()
