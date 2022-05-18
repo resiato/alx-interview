1d28715e1b9dde4e0d2eb4dd167f2a0336ea3d19
resiato <resiatolaitete@gmail.com>
2022-05-18 14:31:19 +0300

    Update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/?format=json';
-request.get(url, function (response, body) {
-  const film = (JSON.parse(body.body));
-  // debugging
-  // console.log(film);
-  // console.log(film.characters);
-  character(film.characters);
-});
-const character = (people, i = 0) => {
-  if (i === people.length) return;
-  request.get(people[i], function (response, body) {
-    // console.log(people[i]);
-    console.log(JSON.parse(body.body).name);
-    character(people, i + 1);
+request.get(url, async (error, res) {
+  if (error) console.log(error);
+  else {
+    const person = JSON.parse(res.body);
+    film(list.characters, 0);
+  }
+  });
+const film = (person, index) => {
+   request.get(person[index], (error, res, body) => {
+    if (error) throw error;
+    const result = JSON.parse(body);
+    console.log(result.name);
+    if (index < person.length - 1) {
+      Execute(person, index + 1);
+    }
   });
 };
