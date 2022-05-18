c245736f3344d4289c517af43339839851b2d423
resiato <resiatolaitete@gmail.com>
2022-05-18 14:54:20 +0300

    [Fix] Remove naming errors

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,14 +1,19 @@
 #!/usr/bin/node
+// script that prints all characters of a Star Wars Movie
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/?format=json';
+const myArgs = process.argv.splice(2);
+const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0] + '/?format=json';
 request.get(url, async (error, res, body) => {
   if (error) {
     console.log(error);
   } else {
     const person = JSON.parse(body).characters;
+    //debugging
+	  //console.log(person);
+	  //console.log(person.characters);
     const personList = peopleURL => {
       const promise = new Promise((resolve, reject) => {
-        request.get(peopleURL, (error, resp, body) => {
+        request.get(peopleURL, (error, res, body) => {
           if (error) {
             reject(error);
           } else {
