bf54fae71734b8858c6b721e8bb28cef2ee0e6a4
resiato <resiatolaitete@gmail.com>
2022-05-18 14:43:46 +0300

    Include promise and await all characters before printing individual names

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,26 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/?format=json';
-request.get(url, async (error, res) {
-  if (error) console.log(error);
-  else {
-    const person = JSON.parse(res.body);
-    film(list.characters, 0);
+request.get(url, async (error, res, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const person = JSON.parse(body).characters;
+    const personList = peopleURL => {
+      const promise = new Promise((resolve, reject) => {
+        request.get(peopleURL, (error, resp, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(body);
+          }
+        });
+      });
+      return promise;
+    };
+    for (let i = 0; i < person.length; i++) {
+      const result = await personList(person[i]);
+      console.log(JSON.parse(result).name);
+   }
   }
-  });
-const film = (person, index) => {
-   request.get(person[index], (error, res, body) => {
-    if (error) throw error;
-    const result = JSON.parse(body);
-    console.log(result.name);
-    if (index < person.length - 1) {
-      Execute(person, index + 1);
-    }
-  });
-};
+});
