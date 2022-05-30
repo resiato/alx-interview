f0de919ab625024496382636205fb74f33868316
resiato <resiatolaitete@gmail.com>
2022-05-30 09:33:13 +0300

    [EDIT] Modify function

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,35 +1,36 @@
 #!/usr/bin/node
 // script that prints all characters of a Star Wars Movie
-const argv = process.argv;
+const argv = process.argv[2];
 const urlFilm = "https://swapi-api.hbtn.io/api/films/";
 const urlMovie = `${urlFilm}${argv[2]}/`;
 
+const { exit } = require('process');
 const request = require("request");
 
-request(urlMovie, function (error, response, body) {
-  if (error == null) {
-    const fbody = JSON.parse(body);
-    const characters = fbody.characters;
+if (argv === undefined) {
+  exit();
+}
 
-    if (characters && characters.length > 0) {
-      const limit = characters.length;
-      CharRequest(0, characters[0], characters, limit);
-    }
+request(urlMovie + argv, (error, response, body) => {
+  if (error == null) {
+    const fbody = JSON.parse(body)
+    const characters = fbody.characters;
+    printIt(characters, 0);
   } else {
     console.log(error);
   }
 });
 
-function CharRequest(idx, url, characters, limit) {
-  if (idx === limit) {
+function printIt(characters, n) {
+  if (n === characters.length) {
     return;
   }
-  request(url, function (error, response, body) {
+  request(characters[n], (error, response, body) => {
     if (!error) {
       const rbody = JSON.parse(body);
-      console.log(rbody.name);
-      idx++;
-      CharRequest(idx, characters[idx], characters, limit);
+      const name = rbody.name;
+      console.log(name);
+      printIt(characters, n + 1);
     } else {
       console.error("error:", error);
     }
